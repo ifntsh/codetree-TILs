433148f5ffe7893d983110cd9afd0e9c5b8b19a2
ifntsh <99662127+ifntsh@users.noreply.github.com>
2024-08-13 15:37:21 +0900

    Title: 길이 단위 변환하기, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

--- 240813/길이 단위 변환하기/change-length-unit.java
+++ 240813/길이 단위 변환하기/change-length-unit.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        System.out.println("9.2ft = 280.4cm");
-        System.out.println("1.3mi = 209214.2cm");
+        // 변수 선언
+        double a = 9.2;
+	    double b = 1.3;
+
+        // 출력
+        System.out.printf("%.1fft = %.1fcm\n%.1fmi = %.1fcm", a, a*30.48, b, b*160934);
     }
 }
