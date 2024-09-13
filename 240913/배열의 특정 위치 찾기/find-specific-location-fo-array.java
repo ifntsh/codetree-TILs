a205bc75d90dfd1364287d54e431b1d429c774de
ifntsh <99662127+ifntsh@users.noreply.github.com>
2024-09-13 10:03:23 +0900

    Title: 배열의 특정 위치 찾기, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240913/배열의 특정 위치 찾기/find-specific-location-fo-array.java
+++ 240913/배열의 특정 위치 찾기/find-specific-location-fo-array.java
@@ -3,22 +3,28 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+
         int[] arr = new int[10];
-        int sum = 0;
-        double avg = 0;
+        int sum2 = 0;
+        int sum3 = 0;
         int cnt = 0;
-        for(int i = 0; i<10; i++){
-            int a = sc.nextInt();
+
+        for(int i = 0; i < 10; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
             if ((i+1) % 2 == 0) {
-                sum += a;
+                sum2 += arr[i];
             }
             if ((i+1) % 3 == 0) {
-                avg += a;
+                sum3 += arr[i];
                 cnt++;
             }
         }
-        avg = avg/cnt;
+
+        double avg3 = (double) sum3 / cnt;
     
-        System.out.printf("%d %.1f", sum, avg);
+        System.out.printf("%d %.1f", sum2, avg3);
     }
 }
